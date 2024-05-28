6c93958d72dd804a657469c6f289e3123108e46c
ferreirabarros <alexandreferreirabarros@hotmail.com>
2024-05-27 23:49:54 -0300

    finalizando javascript

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ window.onload = async () => {
     console.log(error)
     alert('Erro ao carregar cards')
   }
+
+  const nextButton = document.getElementById('next-button')
+  const backButton = document.getElementById('back-button')
+
+  nextButton.addEventListener('click', loadNextPage)
+  backButton.addEventListener('click', loadPreviousPage)
+
 }
 
 async function loadCharacters(url){
@@ -18,7 +25,7 @@ async function loadCharacters(url){
 
     responseJson.results.forEach((character) => {
        const card = document.createElement("div")
-       card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/1.jpg')`
+       card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g,"")}.jpg')`
        card.className = "cards"
 
        const characterNameBG = document.createElement("div")
@@ -32,8 +39,59 @@ async function loadCharacters(url){
       characterNameBG.appendChild(characterName)
       card.appendChild(characterNameBG)
 
+      card.onclick = () =>{
+        const modal = document.getElementById('modal')
+        modal.style.visibility = "visible"
+        
+        const modalContent = document.getElementById('modal-content')
+        modalContent.innerHTML = ''
+
+        const characterImage = document.createElement('div')
+        characterImage.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g,"")}.jpg')`
+        characterImage.className = "character-image"
+
+        const name = document.createElement('span')
+        name.className="character-details"
+        name.innerHTML = `Nome: ${character.name}`
+
+
+        const characterHeight = document.createElement('span')
+        characterHeight.className="character-details"
+        characterHeight.innerHTML = `Altura: ${convertHeight(character.height)}`
+
+        const mass = document.createElement('span')
+        mass.className="character-details"
+        mass.innerHTML = `Peso: ${convertMass(character.mass)}`
+
+        const eyeColor = document.createElement('span')
+        eyeColor.className="character-details"
+        eyeColor.innerHTML = `Cor dos olhos: ${convertEyeColor(character.eye_color)}`
+
+        const birthYear = document.createElement('span')
+        birthYear.className="character-details"
+        birthYear.innerHTML = `Nacimento: ${convertBirthYear(character.birth_year)}`
+
+
+        modalContent.appendChild(characterImage)
+        modalContent.appendChild(name)
+        modalContent.appendChild(characterHeight)
+        modalContent.appendChild(mass)
+        modalContent.appendChild(eyeColor)
+        modalContent.appendChild(birthYear)
+
+      }
+
       mainContent.appendChild(card)
     })
+
+    const nextButton = document.getElementById('next-button')
+    const backButton = document.getElementById('back-button')
+
+    nextButton.disabled = !responseJson.next
+    backButton.disabled = !responseJson.previous
+
+    backButton.style.visibility = responseJson.previous? "visible":"hidden"
+    nextButton.style.visibility = responseJson.next? "visible":"hidden"
 
     currentPageUrl = url
 
@@ -41,4 +99,81 @@ async function loadCharacters(url){
     alert('Erro ao carregar os personagens')
     console.log(error)
   }
+}
+
+async function loadNextPage(){
+  if(!currentPageUrl) return;
+
+  try{
+    const response = await fetch(currentPageUrl)
+    const responseJson = await response.json()
+
+    await loadCharacters(responseJson.next)
+
+  }catch(error){
+    console.log(error)
+    alert('Erro ao carregar a próxima página')
+  }
+}
+
+async function loadPreviousPage(){
+  if(!currentPageUrl) return;
+
+  try{
+    const response = await fetch(currentPageUrl)
+    const responseJson = await response.json()
+
+    await loadCharacters(responseJson.previous)
+
+  }catch(error){
+    console.log(error)
+    alert('Erro ao carregar a página anterior')
+  }
+}
+
+
+function hideModal(){
+  const modal = document.getElementById('modal')
+  modal.style.visibility = "hidden"
+}
+
+function convertEyeColor(eyeColor){
+  const cores = {
+    blue: "azul",
+    brown: "castanhos",
+    green:"verde",
+    yellow:"amarelo",
+    black:"preto",
+    pink:"roda",
+    red:"vermelho",
+    orange:"laranja",
+    hazel:"avela",
+    unknown:"desconhecida"
+  }
+
+  return cores[eyeColor.toLowerCase()] || eyeColor
+}
+
+function convertHeight(height){
+  if(height== "unknown"){
+    return "desconhecida"
+  }
+
+  return (height/100).toFixed(2)
+}
+
+function convertMass(mass){
+  if(mass== "unknown"){
+    return "desconhecido"
+  }
+
+  return `${mass} kg`
+}
+
+function convertBirthYear(birthYear){
+  if(birthYear== "unknown"){
+    return "desconhecido"
+  }
+
+  return birthYear
 }
